5bd5910388f7cf7a39b68c3956f9042a279a4168
Singh <prajsing@amd.com>
2025-06-15 23:17:30 +0530

    Added epsilon greedy action search

--- Flappy_Bird_Gym_Env/agent.py
+++ Flappy_Bird_Gym_Env/agent.py
@@ -1,11 +1,12 @@
 import flappy_bird_gymnasium
 import gymnasium
 from dqn import DQN
-from replay_memory_buffer import ReplayMemory
+from replay_memory_buffer import ReplayMemoryBuffer
 import yaml
 import torch
 import itertools
 import os
+import random
 
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -34,33 +35,46 @@ class Agent:
         
     #We will use this for both training and testing
     def run(self, is_training=True, render=False):
-        env = gymnasium.make("FlappyBird-v0", render_mode="human" if render else None)
+        #env = gymnasium.make("FlappyBird-v0", render_mode="human" if render else None)
+        env = gymnasium.make("CartPole-v1", render_mode="human" if render else None)
+
 
         num_states = env.observation_space.shape[0]
         num_actions = env.action_space.n
 
         rewards_per_episode = []
+        epsilon_history = []
 
         policy_DQN = DQN(num_states, num_actions).to(device)
 
         if is_training:
             memory = ReplayMemoryBuffer(max_size=self.replay_buffer_size)
+            epsilon = self.epsilon_start
 
         for episode in itertools.count():
             state, _ = env.reset()
+            state = torch.tensor(state, dtype=torch.float32, device=device)
+
             terminated = False
             episode_reward = 0
 
             ## We will train indefinitely, and close the environment manually based on rewards per episode data.
             while not terminated:
                 # Next action:
-                # (feed the observation to your agent here)
-                action = env.action_space.sample()      #TODO: Currently taking actions ramdomly, fix this with epsilon-greedy.
+                if is_training and random.random() < epsilon:
+                    action = env.action_space.sample()
+                    action = torch.tensor(action, dtype=torch.int64, device=device) # Convert action to tensor
+                else:
+                    with torch.no_grad():
+                        action = policy_DQN(state.unsqueeze(dim=0)).squeeze().argmax() # we do squeeze to make it 1D and we do item() to convert it back to integer from tensor
 
                 # Processing:
-                new_state, reward, terminated, _, info = env.step(action)
+                new_state, reward, terminated, _, info = env.step(action.item())
                 
                 episode_reward += reward
+
+                new_state = torch.tensor(new_state, dtype=torch.float32, device=device)
+                reward = torch.tensor(reward, dtype=torch.float32, device=device)
 
                 if is_training:
                     memory.append((state, action, reward, new_state, terminated))
@@ -69,8 +83,10 @@ class Agent:
 
             rewards_per_episode.append(episode_reward)
 
+            epsilon = max(self.epsilon_min, epsilon * self.epsilon_decay)
+            epsilon_history.append(epsilon)
 
 
 if __name__ == '__main__':
-    agent = Agent()
-    agent.run(render=True) #Set render to true to see the agent play, and false to train faster
+    agent = Agent(hyperparameter_set='cartpole1')
+    agent.run(is_training=True, render=True) #Set render to true to see the agent play, and false to train faster
