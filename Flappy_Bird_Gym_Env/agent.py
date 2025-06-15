a69b03762e0e2e40af65502f1cb52326aa89edbc
Singh <prajsing@amd.com>
2025-06-15 21:52:21 +0530

    Made a run method for training, to extend

--- Flappy_Bird_Gym_Env/agent.py
+++ Flappy_Bird_Gym_Env/agent.py
@@ -1,5 +1,22 @@
 import flappy_bird_gymnasium
 import gymnasium
+from dqn import DQN
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+class Agent:
+
+    #We will use this for both training and testing
+    def run(self, is_training=True, render=False):
+        env = gymnasium.make("FlappyBird-v0", render_mode="human" if render else None)
+
+        num_states = env.observation_space.shape[0]
+        num_actions = env.action_space.n
+
+        policy_DQN = DQN(num_states, num_actions).to(device)
+
+
+
 env = gymnasium.make("FlappyBird-v0", render_mode="human", use_lidar=True)
 
 obs, _ = env.reset()
